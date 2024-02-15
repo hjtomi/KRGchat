9f5c3d7087fff56d294974d4061c0e50468df2fe
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2024-02-15 22:57:42 +0100

    same username can't be active at the same time, junk commented code removed

--- krgchatnew.py
+++ krgchatnew.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, make_response, flash
+from flask import Flask, render_template, session, url_for, request, redirect, make_response
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -8,6 +8,7 @@ from flask_socketio import SocketIO, emit
 import datetime
 import os
 from dotenv import load_dotenv
+from colorama import Fore, Style
 
 load_dotenv()
 db = SQLAlchemy()
@@ -17,6 +18,8 @@ app.config["SECRET_KEY"] = os.environ.get('KRGCHAT_SECRET_KEY')
 bootstrap = Bootstrap5(app)
 socket = SocketIO(app)
 db.init_app(app)
+
+online_members = {}
 
 
 class FormUsername(FlaskForm):
@@ -40,6 +43,8 @@ def home():
     username = request.cookies.get('username', '')
     if not username:
         return redirect(url_for('set_username'))
+    if username in online_members.values():
+        return redirect(url_for('set_username'))
     messages = db.session.query(Message).all()
     return render_template('index.html', messages=messages, username=username)
 
@@ -56,19 +61,6 @@ def set_username():
 
     return render_template('name.html', form=FormUsername())
 
-# Junk, not used (didnt delete because i am not 100% sure)
-# @app.route("/send_message/<username>", methods=["GET", "POST"])
-# def send_message(username):
-#     new_record = Message(
-#         message=request.form["message"],
-#         time=datetime.datetime.now().replace(microsecond=0),
-#         sender=username
-#     )
-#     db.session.add(new_record)
-#     db.session.commit()
-#     emit('message', {'data': 'new message sent!'})
-#     return redirect(url_for('home', username=username))
-
 
 @app.template_filter('format_date')
 def format_date_filter(str_date):
@@ -78,6 +70,7 @@ def format_date_filter(str_date):
 
 @socket.on('message')
 def hangle_msg(msg):
+    print(online_members)
     time = datetime.datetime.now().replace(microsecond=0)
     new_message = Message(
         message=msg['message'],
@@ -92,6 +85,27 @@ def hangle_msg(msg):
         'username': msg['username'],
         'time': format_date_filter(str(time))
     }, broadcast=True)
+
+
+@socket.on('connect')
+def connect():
+    session_id = request.sid
+    username = request.cookies.get('username', '')
+    online_members[session_id] = username
+    print(f'newly joined user sid: {Fore.CYAN + session_id + Style.RESET_ALL}')
+    print(f'newly joined user: {username}')
+
+
+@socket.on('disconnect')
+def disconnect():
+    session_id = request.sid
+    username = request.cookies.get('username', '')
+    try:
+        online_members.pop(session_id)
+    except KeyError:
+        print('keyerror in deleting user from online members')
+    print(f'user disconnected: {Fore.CYAN + session_id + Style.RESET_ALL}')
+    print(f'left user: {username}')
 
 
 app.jinja_env.filters['format_date'] = format_date_filter
