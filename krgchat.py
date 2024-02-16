b6e93caf4f7c3e3f597b9449dab07afe6d91d06e
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2024-02-16 11:31:29 +0100

    krgchatnew is the new krgchat

--- krgchat.py
+++ krgchat.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, session, url_for, request, redirect, make_response
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length
 from flask_socketio import SocketIO, emit
 import datetime
 import os
 from dotenv import load_dotenv
-
+from colorama import Fore, Style
 
 load_dotenv()
 db = SQLAlchemy()
@@ -19,14 +19,11 @@ bootstrap = Bootstrap5(app)
 socket = SocketIO(app)
 db.init_app(app)
 
-
-class FormMessage(FlaskForm):
-    message = StringField("", validators=[DataRequired()], render_kw={'autofocus': True})
-    send = SubmitField("Küldés")
+online_members = {}
 
 
 class FormUsername(FlaskForm):
-    username = StringField("", validators=[DataRequired()], render_kw={'autofocus': True})
+    username = StringField("", validators=[InputRequired(), Length(max=20)], render_kw={'autofocus': True})
     submit = SubmitField("Tovább")
 
 
@@ -41,31 +38,28 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    username = request.cookies.get('username', '')
+    if not username:
+        return redirect(url_for('set_username'))
+    if username in online_members.values():
+        return redirect(url_for('set_username'))
+    messages = db.session.query(Message).all()
+    return render_template('index.html', messages=messages, username=username)
+
+
+@app.route("/set-username", methods=["GET", "POST"])
 def set_username():
     if request.method == "POST":
-        return redirect(url_for('home', username=request.form["username"]))
+        username = request.form["username"]
+        if len(username) > 20:
+            return render_template('name.html', form=FormUsername())
+        response = make_response(redirect(url_for('home')))
+        response.set_cookie('username', username, max_age=31536000)
+        return response
 
     return render_template('name.html', form=FormUsername())
-
-
-@app.route("/<username>")
-def home(username):
-    messages = db.session.query(Message).all()
-    return render_template('index.html', messages=messages, form=FormMessage(), username=username)
-
-
-@app.route("/send_message/<username>", methods=["GET", "POST"])
-def send_message(username):
-    new_record = Message(
-        message=request.form["message"],
-        time=datetime.datetime.now().replace(microsecond=0),
-        sender=username
-    )
-    db.session.add(new_record)
-    db.session.commit()
-    emit('message', {'data': 'new message sent!'})
-    return redirect(url_for('home', username=username))
 
 
 @app.template_filter('format_date')
@@ -76,6 +70,7 @@ def format_date_filter(str_date):
 
 @socket.on('message')
 def hangle_msg(msg):
+    print(online_members)
     time = datetime.datetime.now().replace(microsecond=0)
     new_message = Message(
         message=msg['message'],
@@ -92,7 +87,28 @@ def hangle_msg(msg):
     }, broadcast=True)
 
 
+@socket.on('connect')
+def connect():
+    session_id = request.sid
+    username = request.cookies.get('username', '')
+    online_members[session_id] = username
+    print(f'newly joined user sid: {Fore.CYAN + session_id + Style.RESET_ALL}')
+    print(f'newly joined user: {username}')
+
+
+@socket.on('disconnect')
+def disconnect():
+    session_id = request.sid
+    username = request.cookies.get('username', '')
+    try:
+        online_members.pop(session_id)
+    except KeyError:
+        print('keyerror in deleting user from online members')
+    print(f'user disconnected: {Fore.CYAN + session_id + Style.RESET_ALL}')
+    print(f'left user: {username}')
+
+
 app.jinja_env.filters['format_date'] = format_date_filter
 
 if __name__ == "__main__":
-    socket.run(app)
+    socket.run(app, debug=True, host='192.168.1.82', port=8080)
